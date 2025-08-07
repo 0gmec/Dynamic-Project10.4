c2229b01de6307dea6d6216499c86eb2b9ffbabd
mlsfiles75@gmail.com <mlsfiles75@gmail.com>
2025-08-07 14:43:23 -0400

    updated component by displaying posts

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,13 +1,20 @@
-
+import {useState} from 'react'
+import { posts } from "../lib/posts"
 
 export default function BlogList() {
+const [blogPosts, setBlogPosts]= useState(posts)
 
-
-
+console.log(blogPosts)
 
     return (
         <div>
             <h2>Blog Post</h2>
+            <ul>
+
+            {blogPosts.map((post)=>
+                 <li>{post.title}</li>)}
+            </ul>
+            
         </div>
 
 )
